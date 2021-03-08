0a8b184cf9b1230af408ea8f67eb9dd67867aec7
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-03-08 18:42:57 +0530

    [batch] Comments and minor edits for batch files

--- Skynet_System/__init__.py
+++ Skynet_System/__init__.py
@@ -24,45 +24,45 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    RAW_SIBYL = os.environ.get("SIBYL", "")
+    RAW_Skynet = os.environ.get("Skynet", "")
     RAW_ENFORCERS = os.environ.get("ENFORCERS", "")
-    SIBYL = list(int(x) for x in os.environ.get("SIBYL", "").split())
+    Skynet = list(int(x) for x in os.environ.get("Skynet", "").split())
     INSPECTORS = list(int(x) for x in os.environ.get("INSPECTORS", "").split())
     ENFORCERS = list(int(x) for x in os.environ.get("ENFORCERS", "").split())
     MONGO_DB_URL = os.environ.get("MONGO_DB_URL")
-    Sibyl_logs = int(os.environ.get("Sibyl_logs"))
-    Sibyl_approved_logs = int(os.environ.get("Sibyl_Approved_Logs"))
+    Skynet_logs = int(os.environ.get("Skynet_logs"))
+    Skynet_approved_logs = int(os.environ.get("Skynet_Approved_Logs"))
     GBAN_MSG_LOGS = int(os.environ.get("GBAN_MSG_LOGS"))
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
 else:
-    import Sibyl_System.config as Config
+    import Skynet_System.config as Config
 
     API_ID_KEY = Config.API_ID
     API_HASH_KEY = Config.API_HASH
     STRING_SESSION = Config.STRING_SESSION
     MONGO_DB_URL = Config.MONGO_DB_URL
-    with open(os.path.join(os.getcwd(), "Sibyl_System/elevated_users.json"), "r") as f:
+    with open(os.path.join(os.getcwd(), "Skynet_System/elevated_users.json"), "r") as f:
         data = json.load(f)
-    SIBYL = data["SIBYL"]
+    Skynet = data["Skynet"]
     ENFORCERS = data["ENFORCERS"]
     INSPECTORS = data["INSPECTORS"]
-    Sibyl_logs = Config.Sibyl_logs
-    Sibyl_approved_logs = Config.Sibyl_approved_logs
+    Skynet_logs = Config.Skynet_logs
+    Skynet_approved_logs = Config.Skynet_approved_logs
     GBAN_MSG_LOGS = Config.GBAN_MSG_LOGS
     BOT_TOKEN = Config.BOT_TOKEN
 
-INSPECTORS.extend(SIBYL)
+INSPECTORS.extend(Skynet)
 ENFORCERS.extend(INSPECTORS)
 
 session = aiohttp.ClientSession()
 
 MONGO_CLIENT = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
 
-from .client_class import SibylClient
+from .client_class import SkynetClient
 
-System = SibylClient(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
+System = SkynetClient(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
 
-collection = MONGO_CLIENT["Sibyl"]["Main"]
+collection = MONGO_CLIENT["Skynet"]["Main"]
 
 
 async def make_collections() -> str:
@@ -89,7 +89,7 @@ async def make_collections() -> str:
     if await collection.count_documents({"_id": 4}, limit=1) == 0:  # Rank tree list
         sample_dict = {"_id": 4, "data": {}, "standalone": {}}
         sample_dict["data"] = {}
-        for x in SIBYL:
+        for x in Skynet:
             sample_dict["data"][str(x)] = {}
             sample_dict["standalone"][str(x)] = {
                 "added_by": 777000,
@@ -101,7 +101,7 @@ async def make_collections() -> str:
 
 def system_cmd(
     pattern=None,
-    allow_sibyl=True,
+    allow_Skynet=True,
     allow_enforcer=False,
     allow_inspectors=False,
     allow_slash=True,
@@ -112,12 +112,12 @@ def system_cmd(
         args["pattern"] = re.compile(r"[\?\.!/]" + pattern)
     else:
         args["pattern"] = re.compile(r"[\?\.!]" + pattern)
-    if allow_sibyl and allow_enforcer:
+    if allow_Skynet and allow_enforcer:
         args["from_users"] = ENFORCERS
-    elif allow_inspectors and allow_sibyl:
+    elif allow_inspectors and allow_Skynet:
         args["from_users"] = INSPECTORS
     else:
-        args["from_users"] = SIBYL
+        args["from_users"] = Skynet
     if force_reply:
         args["func"] = lambda e: e.is_reply
     return events.NewMessage(**args)
