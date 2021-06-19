fcd80240cf0ce0ac8525cb237b346a69a26762f5
Awesome Rajkumar <71130660+Awesome-RJ@users.noreply.github.com>
2021-06-19 15:18:13 +0530

    Shit

--- Skynet_System/__main__.py
+++ Skynet_System/__main__.py
@@ -112,7 +112,7 @@ async def main():
             msg += f"\n**{plugin}**\n\n`{FAILED_TO_LOAD[plugin]}`"
         await System.send_message(Skynet_logs, msg)
     else:
-        await System.send_message(Skynet_logs, "I Here To G-Ban YOu")
+        await System.send_message(Skynet_logs, "I Am Here To G-Ban You")
     await System.run_until_disconnected()
 
 
