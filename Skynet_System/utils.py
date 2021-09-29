95ab53ed52170f6698073a556e6e4bb92fe84962
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-29 15:03:23 +0530

    Fixed

--- Skynet_System/utils.py
+++ Skynet_System/utils.py
@@ -1,14 +1,52 @@
+   
 import re
-from typing import Dict, Tuple
+import shlex
 
+from io import StringIO
+from typing import Dict, Tuple
+from argparse import ArgumentParser
 
 FLAG_REGEX = re.compile(r"-\w+")
 
 
-def seprate_flags(message: str) -> Tuple[Dict[str, bool], str]:
-    flags = FLAG_REGEX.findall(message)
-    flags_dict = {}
-    for flag in flags:
-        flags_dict[flag[1:]] = True
-    message = FLAG_REGEX.sub("", message)
-    return (flags_dict, message)
+class Flag:
+    def __init__(self, prefix, help, action=None, nargs=None, default="dummy_default"):
+        self.args = (prefix,)
+        self.kwargs = {"help": help}
+        if action:
+            self.kwargs["action"] = action
+        if nargs:
+            self.kwargs["nargs"] = nargs
+        if default != "dummy_default":
+            self.kwargs["default"] = default
+
+
+class ParseError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class ArgumentParser(ArgumentParser):
+    def error(self, message):
+        raise ParseError(message)
+
+
+class FlagParser:
+    def __init__(self, flags, desc):
+        self.parser = ArgumentParser(add_help=False, description=desc)
+        for flag in flags:
+            self.parser.add_argument(*flag.args, **flag.kwargs)
+        self.parser.add_argument("-h", "--help", help="Display this message.", action="store_true")
+
+    def parse(self, text, known=False):
+        text = shlex.split(text)
+        return self.parser.parse_known_args(text) if known else self.parser.parse_args(text)
+
+    def get_help(self):
+        string = StringIO()
+        self.parser.print_help(string)
+        return string.getvalue()
+
+
+def seprate_flags(message: str) -> str:
+    return FLAG_REGEX.sub("", message)
