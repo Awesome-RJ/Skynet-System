136fbc1c42e9b830d373bd09321267c89acef397
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-29 15:49:58 +0530

    typo

--- Skynet_System/__init__.py
+++ Skynet_System/__init__.py
@@ -64,7 +64,7 @@ MONGO_CLIENT = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URL)
 from .client_class import SkynetClient
 
 #try:
-    System = SkynetClient(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
+System = SkynetClient(StringSession(STRING_SESSION), API_ID_KEY, API_HASH_KEY)
 #except:
  #   print(traceback.format_exc())
   #  exit(1)
