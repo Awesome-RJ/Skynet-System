cb329fb605f07428e3bd71fc6a8893651bff4df5
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-29 14:07:04 +0530

    Anomalous backslash detected

--- Skynet_System/plugins/bot.py
+++ Skynet_System/plugins/bot.py
@@ -132,7 +132,7 @@ async def callback_handler(event):
             await event.respond(msg)
             await message.edit(
                 re.sub(
-                    "(\*\*)?(Scan)? ?Reason:(\*\*)? (`([^`]*)`|.*)",
+                    r"(\*\*)?(Scan)? ?Reason:(\*\*)? (`([^`]*)`|.*)",
                     f"**Scan Reason:** {r.message}",
                     message.message,
                 )
