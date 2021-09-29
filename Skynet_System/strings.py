46cc6e11b67d805d888f80fedadeb781d251969d
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-29 16:40:26 +0530

    some new

--- Skynet_System/strings.py
+++ Skynet_System/strings.py
@@ -1,34 +1,34 @@
 on_string = """
-Connected to Skynet System! 
-Welcome to Skynet System • Server Core
-⭐⭐⭐⭐⭐
-RANK:{Enforcer}
-NAME : {name}
-You are an authorized user!
+╒═══「 Skynet X System • Server Core 」
+➖➖➖➖➖➖➖➖➖
+➛ NAME: {name}
+➛ RANK: {Enforcer}
+➖➖➖➖➖➖➖➖➖
+╘══「 You are an authorized user! 」
 """
 
 # Make sure not to change these too much
 # If you still wanna change it change the regex too
 scan_request_string = """
-$SCAN
-Cymatic Scan request!
-**Enforcer:** {enforcer} 
-**User scanned:** {spammer}
-**Reason:** `{reason}`
-**Scan Source:** {chat}
-**Target Message:** `{message}`
+╒═══「 $SCAN 」
+Skynet X Scan request!
+**❂ Enforcer:** {enforcer} 
+**❂ User scanned:** {spammer}
+**❂ Reason:** `{reason}`
+**❂ Scan Source:** {chat}
+**❂ Target Message:** `{message}`
 """
 forced_scan_string = """
-$FORCED
-**Inspector:** {ins}
-**Target:** {spammer}
-**Reason:** `{reason}`
-**Scan Source:** {chat}
-**Target Message:** `{message}`
+╒═══「 $FORCED 」
+**❂ Inspector:** {ins}
+**❂ Target:** {spammer}
+**❂ Reason:** `{reason}`
+**❂ Scan Source:** {chat}
+**❂ Target Message:** `{message}`
 """
 
 reject_string = """
-$REJECTED
+╒═══「 $REJECTED 」
 **Crime Coefficient:** `Under 100`
 Not a target for enforcement action. 
 The trigger will be locked.
@@ -42,19 +42,19 @@ proof_string = """
          ┗━[DelDog]({url})"""
 
 scan_approved_string = """
-#LethalEliminator
-**Target User:** {scam}
-**Crime Coefficient:** `Over 300`
-**Reason:** `{reason}`
-**Enforcer:** `{enforcer}`
-**Case Number:** `{proof_id}`
+╒═══「 #LethalEliminator 」
+**❂ Target User:** {scam}
+**❂ Crime Coefficient:** `Over 300`
+**❂ Reason:** `{reason}`
+**❂ Enforcer:** `{enforcer}`
+**❂ Case Number:** `{proof_id}`
 """
 
 bot_gban_string = """
-#DestroyDecomposer
-**Enforcer:** `{enforcer}`
-**Target User:** {scam}
-**Reason:** `{reason}`
+╒═══「 #DestroyDecomposer 」
+**❂ Enforcer:** `{enforcer}`
+**❂ Target User:** {scam}
+**❂ Reason:** `{reason}`
 """
 
 # https://psychopass.fandom.com/wiki/Crime_Coefficient_(Index)
