27db4ca6f2b828ae6034261300ec1ed317e070d9
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-09-29 14:28:49 +0530

    Bans Code Added

---
+++ Skynet_System/plugins/bancodes.py
@@ -0,0 +1,26 @@
+   
+help_plus = """ Here is Help for **Bancodes** :-
+**❂** `{SXSx00}` - **SCAMMER.**
+**❂** `{SXSx01}` - **SPAM ADDING MEMBER.**
+**❂** `{SXSx03}` - **ABUSE SPAM.**
+**❂** `{SXSx04}` - **NSFW SPAMMER.**
+**❂** `{SXSx06}` - **IMPERSONATION.**
+**❂** `{SXSx07}` - **MD/BTC SCAM.**
+**❂** `{SXSx08}` - **ADDING SPAMBOTS.**
+**❂** `{SXSx10}` - **ILLEGAL.**
+**❂** `{SXSx11}` - **PHISHING.**
+**❂** `{SXSx12}` - **FRAUD PROMOTION  [ANY KIND].**
+**❂** `{SXSx13}` - **CYBER THREATENING / CYBER BULLY.**
+**❂** `{SXSx14}` - **CHILD ABUSE.**
+**❂** `{SXSx15}` - **BAN EVASION.**
+**❂** `{SXSx16}` - **SPAMBOT.**
+**❂** `{SXSx17}` - **RAID INITIALIZOR.** 
+**❂** `{SXSx18}` - **RAID PARTICIPANT.**
+**❂** `{SXSx19}` - **KRIMINALANT.**
+**❂** `{SXSx20}` - **SPAMMING VIOLENT CONTENT.**
+**❂** `{SXSx21}` - **HATE SPEECH AGAINST NATIONALITY.**
+**❂** `{SXSx22}` - **RAID/SPAM INFLAMMER.**
+**❂** `{SXSx23}` - **PRONOGRAPHY CCONTENT PROMOTING.**
+**❂** `{SXSx24}` - **PAID GIRL.**
+"""
+__plugin_name__ = "bancodes"
