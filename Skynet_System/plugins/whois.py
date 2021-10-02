0c8011c1f7f6c6d689daae5521c6fd906be76220
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-10-02 21:24:35 +0530

    Update whois.py

--- Skynet_System/plugins/whois.py
+++ Skynet_System/plugins/whois.py
@@ -23,7 +23,7 @@ async def whois(event):
         return
     await System.send_message(
         event.chat_id,
-        f"╒═══「<b> Appraisal results:</b> 」\n❂ First Name: `{data.user.last_name}`\n❂ Last Name: `{data.user.first_name}`\n❂ Username: @{data.user.username}\n❂ Userlink: [{data.user.first_name}](tg://user?id={data.user.id})\n❂ User ID: `{data.user.id}`\n❂ About: {data.about}",
+        f"╒═══「 Appraisal results: 」\n❂ First Name: `{data.user.last_name}`\n❂ Last Name: `{data.user.first_name}`\n❂ Username: @{data.user.username}\n❂ Userlink: [{data.user.first_name}](tg://user?id={data.user.id})\n❂ User ID: `{data.user.id}`\n❂ About: {data.about}",
     )
 
 
